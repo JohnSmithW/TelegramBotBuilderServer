44c4ce584dbb498c5bc92d5c726158c890de831d
JohnSmithW <salapin.danil1@mail.ru>
2021-05-16 18:47:03 +0300

    Added bot scheme handler function

--- controllers/bot.js
+++ controllers/bot.js
@@ -1,19 +1,69 @@
 const { Telegraf } = require('telegraf');
 
-exports.createBot = async (req, res) => {
-  const token = res.token;
-  if (token) {
-    const bot = new Telegraf(token);
-    bot.start(ctx => ctx.reply('Welcome'));
-    bot.help(ctx => ctx.reply('Send me a sticker'));
-    bot.on('sticker', ctx => ctx.reply('👍'));
-    bot.hears('hi', ctx => ctx.reply('Hey there'));
-    bot.hears('wow', ctx => ctx.reply('I know'));
-    bot.launch();
+const buildScheme = (token, data) => {
+  const bot = new Telegraf(token);
+  let scheme = [];
 
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  scheme = data.map(action => {
+    let message = '';
+    let event = '';
+    switch (action.type) {
+      case 'START':
+        message = action.payload.message;
+
+        return bot.start(ctx => ctx.reply(message));
+
+      case 'HELP':
+        message = action.payload.message;
+
+        return bot.start(ctx => ctx.reply(message));
+
+      case 'ON':
+        event = action.payload.event;
+        message = action.payload.message;
+
+        return bot.on(event, ctx => ctx.reply(message));
+
+      case 'HEARS':
+        event = action.payload.event;
+        message = action.payload.message;
+
+        return bot.hears(event, ctx => ctx.reply(message));
+
+      default:
+        return;
+    }
+  });
+
+  scheme.push(
+    bot.launch(),
+    process.once('SIGINT', () => bot.stop('SIGINT')),
+    process.once('SIGTERM', () => bot.stop('SIGTERM')),
+  );
+
+  return scheme;
+};
+
+exports.createBot = async (req, res) => {
+  const { token, scheme } = req.body;
+  console.log(scheme);
+  // const bot = new Telegraf(token);
+
+  if (token) {
+    // bot.start(ctx => ctx.reply('Welcome'));
+    // bot.help(ctx => ctx.reply('Send me a sticker'));
+    // bot.on('sticker', ctx => ctx.reply('👍'));
+    // bot.hears('hi', ctx => ctx.reply('Hey there'));
+    // bot.hears('wow', ctx => ctx.reply('I know'));
+    // bot.launch();
+
+    // process.once('SIGINT', () => bot.stop('SIGINT'));
+    // process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+    buildScheme(token, scheme);
+
+    // console.log(buildScheme(token, scheme));
   }
 
-  res.send({ok:true})
+  res.send({ ok: true });
 };
--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const server = require('http').Server(app);
 
-
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 const showRoutes = require('./routes/index.js');
 
 app.use('/api', showRoutes(app));
-
-
 
 server.listen(PORT, err => {
   if (err) throw err;
